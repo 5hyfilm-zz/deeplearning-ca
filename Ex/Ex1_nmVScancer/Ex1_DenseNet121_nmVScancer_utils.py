ddcb6e80b0195a92696a0cea013be53ea6aa58d2
Waranthorn Chansawang <waranthorn_c@outlook.com>
2021-04-06 14:11:20 +0700

    change name

--- Ex/Ex1_nmVScancer/Ex1_DenseNet121_nmVScancer_utils.py
+++ Ex/Ex1_nmVScancer/Ex1_DenseNet121_nmVScancer_utils.py
@@ -92,7 +92,7 @@ def deprocess_image(x):
     x = np.clip(x, 0, 255).astype('uint8')
     return x
 
-def my_decode_predictions(preds, top=2, class_list_path='/content/AJPom_cancer_data/Ex1_AJPom_cancer_data_index.json'):
+def my_decode_predictions(preds, top=2, class_list_path='/content/AJPom_cancer_data/nmVScancer/nmVScancer_data_index.json'):
   if len(preds.shape) != 2 or preds.shape[1] != 2:
     raise ValueError('`decode_predictions` expects '
                      'a batch of predictions '
